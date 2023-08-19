2bb889ab8dff415a73e3a1d1255038ca4e0b5083
Sebastian Fischer <typo3@evoweb.de>
2023-08-19 13:15:10 +0200

    Cleanup

---
+++ Configuration/Icons.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    't3-form-icon-recaptcha' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:recaptcha/Resources/Public/Images/reCaptcha_sw.svg',
+    ],
+];
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,26 +1,14 @@
 <?php
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 defined('TYPO3') or die();
 
 call_user_func(function () {
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:form/Resources/Private/Language/Database.xlf'][] =
         'EXT:recaptcha/Resources/Private/Language/Backend.xlf';
 
-    /** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-    $iconRegistry->registerIcon(
-        't3-form-icon-recaptcha',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => 'EXT:recaptcha/Resources/Public/Images/reCaptcha_sw.svg']
-    );
-
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('
-module.tx_form {
-    settings {
-        yamlConfigurations {
-            1974 = EXT:recaptcha/Configuration/Yaml/FormSetup.yaml
-        }
-    }
-}
+    ExtensionManagementUtility::addTypoScriptSetup('
+module.tx_form.settings.yamlConfigurations.1974 = EXT:recaptcha/Configuration/Yaml/FormSetup.yaml
     ');
 });
